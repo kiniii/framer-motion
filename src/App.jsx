013cf0e4adb3099e7cea1f80953c51b700c884eb
Kinlok Lau <kinloklau@Kinloks-MacBook-Pro.local>
2024-05-10 15:31:22 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./index.css";
+import "tailwindcss/tailwind.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl font-bold flex">Testing</div>
+    </>
+  );
+}
+
+export default App;
